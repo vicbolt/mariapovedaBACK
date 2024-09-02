994d3d128e21586ed94ef50bb735a42c4b5133d6
vicbolt <vicbolt.madrid@gmail.com>
2024-09-02 09:57:53 +0200

    changes privacy

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
+console.log(process.env)
 
 const express = require('express');
 const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
-const cors = require('cors'); 
+const cors = require('cors');
 
 const app = express();
+
+// Verificación de la carga de variables de entorno
+console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
 
 // Configuración de CORS
 app.use(cors({
@@ -30,9 +35,10 @@ const transporter = nodemailer.createTransport({
 });
 
 app.post('/send-email', (req, res) => {
+
   const { nombre, email, telefono, motivo, mensaje } = req.body;
 
-  // Leer y compilar la plantilla
+  // Leer y compilar la plantilla para María
   const templatePath1 = path.join(__dirname, 'mail', 'master.html');
   fs.readFile(templatePath1, 'utf8', (err, template) => {
     if (err) {
@@ -48,22 +54,24 @@ app.post('/send-email', (req, res) => {
       mensaje
     });
 
-    // Configuración del correo
+    // Configuración del correo para María
     const mailOptions1 = {
       from: 'vicbolt.madrid@gmail.com',
-      to: `vicbolt.madrid@gmail.com`, // Enviar a María
+      to: 'vicbolt.madrid@gmail.com', // Enviar a María
       subject: 'Formulario de Contacto',
       html: htmlToSend
     };
 
     transporter.sendMail(mailOptions1, (error, info) => {
       if (error) {
-        return res.status(500).json('Error al enviar el correo');
+        console.log('Error al enviar correo a María:', error);
+      } else {
+        console.log('Correo enviado a María:', info.response);
       }
-      res.status(200).json('Correo enviado con éxito');
     });
   });
 
+  // Leer y compilar la plantilla para el usuario
   const templatePath2 = path.join(__dirname, 'mail', 'user.html');
   fs.readFile(templatePath2, 'utf8', (err, template) => {
     if (err) {
@@ -79,10 +87,10 @@ app.post('/send-email', (req, res) => {
       mensaje
     });
 
-    // Configuración del correo
+    // Configuración del correo para el usuario
     const mailOptions2 = {
       from: 'vicbolt.madrid@gmail.com',
-      to: `${email}`, // Envio al usuario
+      to: email, // Envio al usuario
       subject: 'Gracias por contactar conmigo.',
       html: htmlToSend
     };
@@ -90,8 +98,10 @@ app.post('/send-email', (req, res) => {
     transporter.sendMail(mailOptions2, (error, info) => {
       if (error) {
         return res.status(500).json('Error al enviar el correo');
+      } else {
+        console.log('Correo enviado al usuario:', info.response);
+        return res.status(200).json('Correo enviado con éxito');
       }
-      res.status(200).json('Correo enviado con éxito');
     });
   });
 });
